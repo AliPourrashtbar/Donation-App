cbd900541fa1e08eef7593b7d7e3ce7b252903ee
AliPourrashtbar <alipourijon208@gmail.com>
2023-10-02 20:51:28 +0330

    feat: :sparkles: add singleItem

---
+++ components/DonateSingleItem/DonateSingleItem.js
@@ -0,0 +1,15 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import style from './style';
+import Header from '../Header/Header';
+import Badge from '../Badge/Badge';
+
+const DonateSingleItem = () => {
+  return (
+    <View>
+      <Text>DonateSingleItem</Text>
+    </View>
+  );
+};
+
+export default DonateSingleItem;
--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,11 +1,8 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import style from './style';
-import Header from '../../components/Header/Header';
-import Button from '../../components/Button/Button';
-import Tab from '../../components/Tab/Tab';
-import Badge from '../../components/Badge/Badge';
 import Search from '../../components/Search/Search';
+
 const Home = () => {
   return (
     <View style={style.container}>
